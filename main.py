73dcf30708b80c55d0925cb831cae4bc6d37f55d
matteo75 <matteoperini01@gmail.com>
2022-07-24 00:17:32 +0200

    Password saving in txt file

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Ask for a victim's information
 name = input("What is your victim's name? ")
 surname = input("What is your victim's surname? ")
-dob = input("What is your victim's date of birth? (GGMMYYYY)")
+dob = input("What is your victim's date of birth? (GGMMYYYY) > ")
 
 dob_g = int(dob[0:2])
 dob_m = int(dob[2:4])
@@ -19,18 +19,26 @@ def check_date(dob):
 def main():
     check_date(dob)
 
-    password = []
+    passwords = []
     # Generate a password combining the victim's name, surname and date of birth
     for char in specialCharacters:
-        password.append(name + dob[4:8] + char)
-        password.append(name + dob[6:8] + char)
-        password.append(surname + dob[4:8] + char)
-        password.append(surname + dob[6:8] + char)
-        password.append(name + surname + dob[4:8] + char)
-        password.append(name + surname + dob[6:8] + char)
-        password.append(surname + name + dob[4:8] + char)
-        password.append(surname + name + dob[6:8] + char)
+        passwords.append(name + dob[4:8] + char)
+        passwords.append(name + dob[6:8] + char)
+        passwords.append(surname + dob[4:8] + char)
+        passwords.append(surname + dob[6:8] + char)
+        passwords.append(name + surname + dob[4:8] + char)
+        passwords.append(name + surname + dob[6:8] + char)
+        passwords.append(surname + name + dob[4:8] + char)
+        passwords.append(surname + name + dob[6:8] + char)
 
-    print(f"Generated {len(password)} passwords for {name} {surname}.\n", password)
+
+    # Save the password to a file
+    with open(f"{name}.txt", "w") as file:
+        for password in passwords:
+            file.write(password)
+            if password != passwords[-1]:
+                file.write("\n")
+    
+    print(f"Generated {len(passwords)} passwords for {name} {surname} in {name}.txt")
 
 main()
